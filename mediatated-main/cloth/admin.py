42b55f935d3b2db5f9d5296884573028cb01ada8
pleb10118-cyber <pleb10118@gmail.com>
2026-03-25 22:17:37 +0000

    kkk

--- mediatated-main/cloth/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Product, Cart, CartItem
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(CartItem)
--- mediatated-main/cloth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClothConfig(AppConfig):
-    name = 'cloth'
